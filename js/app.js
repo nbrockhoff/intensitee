33e7036acae7426d04f86e8357b8031e6df340f4
Noelle Brockhoff <noelle@nbrockhoff.com>
2017-09-18 12:31:25 -0500

    Lunch break - adding in contact form

--- js/app.js
+++ js/app.js
@@ -37,4 +37,21 @@ $(document).ready(function () {
     // instead of a settings object
   ]
     });
+    
+    $(function () {
+  var showClass = 'show';
+
+  $('input').on('checkval', function () {
+    var label = $(this).prev('label');
+    if(this.value !== '') {
+      label.addClass(showClass);
+    } else {
+      label.removeClass(showClass);
+    }
+  }).on('keyup', function () {
+    $(this).trigger('checkval');
+  });
+});
+
+
 });
